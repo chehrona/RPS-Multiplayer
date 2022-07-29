10a26f686689add11a23fc38acaff2bc571234b2
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2022-07-29 17:07:51 -0400

    Proper chat design

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
     $("<audio></audio>").attr({
         'src':'assets/javascript/hello.wav',
-        // 'volume': 0.4,
         'autoplay':'autoplay'
     }).appendTo("body");
-})
+
+    let playerName = "";
+    let playerNameFormatted = "";
+
+    $("#nameEnter").keypress(function (e) {
+        if (e.keyCode == "13") {
+            playerName = $("#nameEnter").val().trim();
+            // $("#nameEnter").html("");   
+            playerNameFormatted = playerName.charAt(0).toUpperCase() + playerName.slice(1);
+            $("#yourName").html(playerNameFormatted);
+        }
+    });
+
+    $("start").on("click", function (e) {
+        e.preventDefault();
+        playerName = $("#nameEnter").val().trim();
+        // $("#nameEnter").text("");
+        playerNameFormatted = playerName.charAt(0).toUpperCase() + playerName.slice(1);
+        $("#yourName").html(playerNameFormatted);
+            
+    });
+
+
+    ("#robotU").on("click", function () {
+        ("#xBorder").removeClass(".borders");
+        ("#xBorder").css({
+            "border": "8px double rgb(0, 255, 251)",
+            "border-radius": "20px",
+            "padding-top": "1%",
+            "display": "block",
+            "animation": "none"});
+    });
+    
+});
