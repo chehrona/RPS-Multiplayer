b2cc73ee959770637ce13f7cf5ea79a817266aa5
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2022-07-30 00:04:12 -0400

    active card selection

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -4,36 +4,71 @@ $(document).ready(function () {
         'autoplay':'autoplay'
     }).appendTo("body");
 
+    let createsModals = function (message) {
+        let modalBox = $("<div class='popup active'></div>");
+        modalBox.append($("<p id='popupContent' class='active'>"+ message + "</p>"));
+        modalBox.append($("<button class='close'>X</button>"));
+        modalBox.appendTo($("playZone"));
+        $(".close").on("click", function() {
+            $(".popup-overlay, p").removeClass("active");
+        });
+    }
+
     let playerName = "";
     let playerNameFormatted = "";
 
     $("#nameEnter").keypress(function (e) {
         if (e.keyCode == "13") {
             playerName = $("#nameEnter").val().trim();
-            // $("#nameEnter").html("");   
+            $(this).val("");
+            $(this).blur();
             playerNameFormatted = playerName.charAt(0).toUpperCase() + playerName.slice(1);
-            $("#yourName").html(playerNameFormatted);
         }
     });
 
     $("start").on("click", function (e) {
         e.preventDefault();
         playerName = $("#nameEnter").val().trim();
-        // $("#nameEnter").text("");
+        $("#nameEnter").val("");
+        $("#nameEnter").blur();
         playerNameFormatted = playerName.charAt(0).toUpperCase() + playerName.slice(1);
-        $("#yourName").html(playerNameFormatted);
-            
     });
 
+    $("#robotU").on("click", function () {
+        if (playerName !== "") {
+            $("#yourName").html(playerNameFormatted);
+        } else {
+            createsModals("Please enter your name");
+        }
 
-    ("#robotU").on("click", function () {
-        ("#xBorder").removeClass(".borders");
-        ("#xBorder").css({
-            "border": "8px double rgb(0, 255, 251)",
-            "border-radius": "20px",
-            "padding-top": "1%",
-            "display": "block",
-            "animation": "none"});
+        $("#robotU").removeClass("bots");
+        $("#robotU").addClass("iconPicked");
+        $("#uBorder").removeClass(".borders");
+        $("#uBorder").addClass("inanimBorder");
+        $("#robotX").addClass("inactiveIcon");
+        $("#xBorder").removeClass(".borders");
+        $("#xBorder").addClass("inactiveBorder");
     });
     
+
+    $("#robotX").on("click", function () {
+        if (playerNameFormatted !== "") {
+            $("#opponentName").html(playerNameFormatted);
+        } else {
+            let modalBox = $("<div id='nameModal' class='modal'>"+
+            "<p>Please enter your name</p></div>");
+            $("playerZone").append(modalBox);
+        }
+        $("#robotX").removeClass("bots");
+        $("#robotX").addClass("iconPicked");
+        $("#xBorder").removeClass(".borders");
+        $("#xBorder").addClass("inanimBorder");
+        $("#robotU").addClass("inactiveIcon");
+        $("#uBorder").removeClass(".borders");
+        $("#uBorder").addClass("inactiveBorder");
+        if (playerNameFormatted !== "") {
+            $("#opponentName").html(playerNameFormatted);
+        }
+    });
+
 });
