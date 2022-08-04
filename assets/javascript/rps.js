3dd165179e7563c00fd95b08d2224787c47d782b
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2022-08-03 21:17:54 -0400

    Allows each player to make a choice, updates losses and wins

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -36,7 +36,17 @@ $(document).ready(function () {
     let playerTwoName = "";
     let playerId = "";
     let spinnerIcon = '<i class="fa-solid fa-spinner fa-spin-pulse biggerIcon" id="spinner"></i>';
-    
+    let checkMark = '<i class="fa-solid fa-check biggerIcon" id="checkMark"></i>'
+    let hasPlayerOneChosen = false;
+    let hasPlayerTwoChosen = false;
+    let playerOneChoice = "";
+    let playerTwoChoice = "";
+    let playerOneWins = 0;
+    let playerTwoWins = 0;
+    let playerOneLosses = 0;
+    let playerTwoLosses= 0;
+
+
     // Db values changed event listener.
     // This function is called everytime players data changes.
     onValue(playersRef, function (snapshot) {
@@ -50,8 +60,9 @@ $(document).ready(function () {
             return;
         }
 
-        let firstPlayerId = "1" in dbData;
-        let secondPlayerId = "2" in dbData;
+        let firstPlayerId = snapshot.child("1").exists();
+        let secondPlayerId = snapshot.child("2").exists();
+
 
         if (firstPlayerId) {
             playerOneScreen = true;
@@ -67,7 +78,7 @@ $(document).ready(function () {
         
         if (secondPlayerId) {
             playerTwoScreen = true;
-            playerTwoName = snapshot.val()["2"].name;
+            playerTwoName = dbData["2"].name;
             isPlayerTwoConnected = true;
             $("#opponentName").html(playerTwoName);
             $("#robotU").addClass("disabled");
@@ -85,6 +96,36 @@ $(document).ready(function () {
             $(".uIcons").addClass("disabled")
         }
         $(".playerIcon").removeClass("highlightPlayerIcon");
+
+        // Retrieves choice data for each player
+        let firstPlayerChoice = snapshot.child("1/choice").exists();
+        let secondPlayerChoice = snapshot.child("2/choice").exists();
+
+        if (firstPlayerChoice) {
+            playerOneChoice = dbData["1"].choice;
+            hasPlayerOneChosen = true;
+            // $("#uBorder").removeClass("currentPlayer");
+            // $("#robotU").removeClass("iconPicked");
+        }
+
+        if (secondPlayerChoice) {
+            playerTwoChoice = dbData["2"].choice;
+            hasPlayerTwoChosen = true;
+            // $("#xBorder").removeClass("currentPlayer");
+            // $("#robotX").removeClass("iconPicked");
+        }
+
+        // Retrieves wins and losses
+        playerOneWins = dbData["1"].wins;
+        playerTwoWins = dbData["2"].wins;
+        playerOneLosses = dbData["1"].losses;
+        playerTwoLosses = dbData["2"].losses;
+
+        $("#uWinCount").html(playerOneWins);
+        $("#uLossCount").html(playerOneLosses);
+        $("#xWinCount").html(playerTwoWins);
+        $("#xLossCount").html(playerTwoLosses);
+
     });
 
     onValue(turnRef, function (snapshot) {
@@ -92,32 +133,42 @@ $(document).ready(function () {
 
         if (playerTurn == "1") {
             $(".playerIcon").css({"pointer-events": "none"});
+            // Removes highlights from the player 2 and adds it to player 1
+            $("#xBorder").removeClass("currentPlayer");
+            $("#robotX").removeClass("iconPicked");
+            $("#uBorder").addClass("currentPlayer");
+            $("#robotU").addClass("iconPicked");
+            $("#uBorder").removeClass("bordersAnim");
             if (playerTurn === playerId) {
-                $("#uBorder").addClass("currentPlayer");
-                $("#robotU").addClass("iconPicked");
-                $("#uBorder").removeClass("bordersAnim");
+                // Highlight player 1 and enable buttons
+                $(".uIcons").removeClass("removeEvent");
                 $("#xBorder").removeClass("bordersAnim");
                 $(".xIcons").addClass("inactiveIcon");
             } else {
-                $("#uBorder").removeClass("bordersAnim");
-                $("#uBorder").addClass("currentPlayer");
-                $("#robotU").addClass("iconPicked");
+                // Highlight player 1 and disable buttons
+                $(".xIcons").addClass("removeEvent");
                 $(".uIcons").addClass("inactiveIcon");
             }
         } else if (playerTurn == "2") {
             $(".playerIcon").css({"pointer-events": "none"});
+            // Removes highlights from the player 1 and adds it to player 2
+            $("#uBorder").removeClass("currentPlayer");
+            $("#robotU").removeClass("iconPicked");
+            $("#xBorder").addClass("currentPlayer");
+            $("#robotX").addClass("iconPicked");
+            $("#xBorder").removeClass("bordersAnim");
             if (playerTurn === playerId) {
-                $("#xBorder").addClass("currentPlayer");
-                $("#robotX").addClass("iconPicked");
-                $("#xBorder").removeClass("bordersAnim");
+                $(".xIcons").removeClass("removeEvent");
                 $("#uBorder").removeClass("bordersAnim");
                 $(".uIcons").addClass("inactiveIcon");
             } else {
-                $("#xBorder").removeClass("bordersAnim");
+                $(".uIcons").addClass("removeEvent");
                 $("#xBorder").addClass("currentPlayer");
-                $("#robotX").addClass("iconPicked");
-                $(".xIcons").addClass("inactiveIcon");
             }
+        }
+
+        if (hasPlayerOneChosen && hasPlayerTwoChosen) {
+            gameRules();
         }
 
     });
@@ -180,6 +231,7 @@ $(document).ready(function () {
         }
 
         playerId = $(this).attr("data-id-player");
+        console.log("Updating turn...");
         
         update(ref(db), {
             turn: playerId
@@ -197,63 +249,99 @@ $(document).ready(function () {
             wins: 0
         });
 
-        onDisconnect(playerRef).remove(); 
+        onDisconnect(playerRef).remove();
+
+        
     });
     
-    $(".rock").on("click", function () {
-        if (isPlayerOneConnected && isPlayerTwoConnected) {
-            $(".playerIcon").addClass("disabled");
-        // } else if (isPlayerOneConnected) {
-        //     createsModals("Wait for player 2 to join");
-        //     $("#yourScreen").addClass("disabled");
-        //     return;
-        // } else if (isPlayerTwoConnected) {
-        //     createsModals("Wait for player 1 to join");
-        //     $("#opponentScreen").addClass("disabled");
-        //     return;
-        }  
-        if ((playerName == "") || (!isPlayerOneConnected) || (!isPlayerTwoConnected)) {
-            createsModals("Please enter your name");
-            return;
-        }
+    $(".playerChoice").on("click", function () {
+        let choice = $(this).attr("data-choice");
 
-        playerId = $(this).attr("data-id-player");
-
-        if (isPlayerOneConnected) {
-            $("#opponentName").html("Waiting...");
-            $("#xIconContainer").append(spinnerIcon);
-
-            $("#uRock").addClass("biggerIcon");
-            $("#uPaper").addClass("inactive");
-            $("#uScissor").addClass("inactive");
-            $("#xPaper").addClass("inactive");
-            $("#xScissor").addClass("inactive");
-            $("#xRock").addClass("inactive");
+        if (playerTurn == "1") {
 
             update(playerOneRef, {
-                choice: "rock"
+                choice: choice
             });
-
-            onDisconnect(playersRef).remove();
+            
+            update(ref(db), {
+                turn: "2"
+            });            
         } else {
-            $("#opponentName").html(playerTwoName);
-            $("#yourName").html("Waiting...");
-            $("#xRock").addClass("biggerIcon");
-            $("#uIconContainer").append(spinnerIcon);
-
-            $("#xPaper").addClass("inactive");
-            $("#xScissor").addClass("inactive");
-            $("#uPaper").addClass("inactive");
-            $("#uScissor").addClass("inactive");
-            $("#uRock").addClass("inactive");
-
             update(playerTwoRef, {
-                choice: "rock"
+                choice: choice
             });
-
-            onDisconnect(playersRef).remove();
-        } 
+            
+            update(ref(db), {
+                turn: "1"
+            });    
+        }
     });
+    //     if (playerId == "1") {
+    //         $("#xIconContainer").append(spinnerIcon);
+    //         pla
+    //         $("#uScissor").addClass("inactive");
+    //         $("#xPaper").addClass("inactive");
+    //         $("#xScissor").addClass("inactive");
+    //         $("#xRock").addClass("inactive");
+
+    //         update(playerOneRef, {
+    //             choice: "rock"
+    //         });
+
+    //         onDisconnect(playersRef).remove();
+
+    //         update(ref(db), {
+    //             turn: "2"
+    //         });
+    //     } else if (playerId == "2") {
+    //         if (hasPlayerOneChosen) {
+    //             $("#uIconContainer").append(checkMark);
+    //         } else {
+    //             $("#IconContainer").append(spinnerIcon);
+    //         }
+            
+    //         $("#xRock").addClass("biggerIcon");
+    //         $("#xPaper").addClass("inactive");
+    //         $("#xScissor").addClass("inactive");
+    //         $("#uPaper").addClass("inactive");
+    //         $("#uScissor").addClass("inactive");
+    //         $("#uRock").addClass("inactive");
+
+    //         update(playerOneRef, {
+    //             choice: "rock"
+    //         });
+
+    //         onDisconnect(playersRef).remove();
+
+    //         update(ref(db), {
+    //             turn: "2"
+    //         });
+    //     }
+
+        
+
+
+
+            
+        // } else {
+        //     $("#opponentName").html(playerTwoName);
+        //     $("#yourName").html("Waiting...");
+        //     $("#xRock").addClass("biggerIcon");
+        //     $("#uIconContainer").append(spinnerIcon);
+
+        //     $("#xPaper").addClass("inactive");
+        //     $("#xScissor").addClass("inactive");
+        //     $("#uPaper").addClass("inactive");
+        //     $("#uScissor").addClass("inactive");
+        //     $("#uRock").addClass("inactive");
+
+        //     update(playerTwoRef, {
+        //         choice: "rock"
+        //     });
+
+        //     onDisconnect(playersRef).remove();
+        // } 
+    
 
 
     // Creates modals based on the message passed to the function.
@@ -263,9 +351,74 @@ $(document).ready(function () {
         $(".popup").addClass("active");
         $("#popupContent").text(message);
         $(".xMark").on("click", function() {
-            $(".popup").removeClass("active");
-            $(".popup").addClass("inactive");
-            $("#playZone").removeClass("inactiveBackground");
+        $(".popup").removeClass("active");
+        $(".popup").addClass("inactive");
+        $("#playZone").removeClass("inactiveBackground");
         });
+    };
+
+    let gameRules = function() {
+        if (playerOneChoice === playerTwoChoice) {
+            console.log("Play again. Tie");
+        } else if (playerOneChoice === "rock" && playerTwoChoice === "paper") {
+            update(playerTwoRef, {
+                wins: playerTwoWins + 1
+            });
+
+            update(playerOneRef, {
+                losses: playerOneLosses + 1
+            });
+
+        } else if (playerOneChoice === "rock" && playerTwoChoice === "scissors") {
+            update(playerOneRef, {
+                wins: playerOneWins + 1
+            });
+
+            update(playerTwoRef, {
+                losses: playerTwoLosses + 1
+            });
+        } else if (playerOneChoice === "paper" && playerTwoChoice === "rock") {
+            update(playerOneRef, {
+                wins: playerOneWins + 1
+            });
+
+            update(playerTwoRef, {
+                losses: playerTwoLosses + 1
+            });
+        } else if (playerOneChoice === "paper" && playerTwoChoice === "scissors") {
+            update(playerTwoRef, {
+                wins: playerTwoWins + 1
+            });
+
+            update(playerOneRef, {
+                losses: playerOneLosses + 1
+            });
+        } else if (playerOneChoice === "scissors" && playerTwoChoice === "rock") {
+            update(playerTwoRef, {
+                wins: playerTwoWins + 1
+            });
+
+            update(playerOneRef, {
+                losses: playerOneLosses + 1
+            });
+
+            playerTwoWins++;
+        } else if (playerOneChoice === "scissors" && playerTwoChoice === "paper") {
+            update(playerOneRef, {
+                wins: playerOneWins + 1
+            });
+
+            update(playerTwoRef, {
+                losses: playerTwoLosses + 1
+            });
+        }
+
+        hasPlayerOneChosen = false;
+        hasPlayerTwoChosen = false;
+
+        // Update turn
+        
+
+        // onDisconnect(playersRef).remove();     
     }
 });
